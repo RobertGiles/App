da7630cf24423bcd78630d46b46ba239525ae65f
Quinn Thorsnes <quinn.thorsnes@gmail.com>
2018-09-17 14:26:40 +1200

    Fixing text boxes
    goal description screen boxes no longer editable or scrollable
    journal button changed to "Confirm Entry" and added example text of "Type Entry Here"

--- App/Xcode/COSC345/WhatisAGoalViewController.swift
+++ App/Xcode/COSC345/WhatisAGoalViewController.swift
@@ -4,7 +4,7 @@ class WhatisAGoalViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
 
